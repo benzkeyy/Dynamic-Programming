f343622004353b02d92f96a432a70ee83b0c2ffd
benzkeyy <54702967+benzkeyy@users.noreply.github.com>
2019-10-17 20:33:55 +0530

    Update uniquepathgrid.cpp

--- uniquepathgrid.cpp
+++ uniquepathgrid.cpp
@@ -1,9 +1,12 @@
 /*
 A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
 The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below). */
-// Basically dp the number of unique paths from top left to bottom right..
+
+
 // We can also use the forumula -  (M+N-2) and get the answer directly!
-//								   ( N-1 ) 
+//				   ( N-1 ) 
+
+
 #include <bits/stdc++.h>
 #define fast std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define endl '\n'
@@ -14,20 +17,20 @@ using namespace std;
 
 int main() 
 { 
-    ll m,n;
+    	ll m,n;
 	cin>>m>>n;
 	int dp[m][n];
 	 
 	for (int i = 0; i < m; i++) 
         dp[i][0] = 1; 
         
-    for (int j = 0; j < n; j++) 
+    	for (int j = 0; j < n; j++) 
         dp[0][j] = 1; 
         
-    for (int i = 1; i < m; i++) 
+    	for (int i = 1; i < m; i++) 
         for (int j = 1; j < n; j++) 
   			dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
   
-    cout << dp[m - 1][n - 1];  
-    return 0; 
+   	cout << dp[m - 1][n - 1];  
+    	return 0; 
 } 
