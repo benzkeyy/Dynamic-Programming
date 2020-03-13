70bb43c059839e2ef5b30e35e2b6ba751bc1f03e
Harshit <54702967+benzkeyy@users.noreply.github.com>
2020-03-13 16:54:20 +0530

    Create LongestIncreasingSubsequence.cpp

---
+++ LongestIncreasingSubsequence.cpp
@@ -0,0 +1,24 @@
+// Longest increasing subsequence using dynamic programming
+// Question - https://leetcode.com/problems/longest-increasing-subsequence
+
+ int lengthOfLIS(vector<int>& nums) 
+    {
+        int n= nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int dp[n];
+        for(int i = 0; i < n; i++)
+            dp[i] = 1;
+        
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[i] > nums[j] && dp[j]+1 > dp[i])
+                {
+                    dp[i] = 1 + dp[j];
+                }
+            }
+        }
+        return *max_element(dp,dp+n);
+    }
