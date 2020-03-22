1d8d10e044a1fb7f7dc7e41033f8cf8af4126d6d
Harshit <54702967+benzkeyy@users.noreply.github.com>
2020-03-23 01:47:05 +0530

    Added rod cutting

--- UnboundedKnapsack.cpp
+++ UnboundedKnapsack.cpp
@@ -3,15 +3,14 @@
 
 int knapsack(int W, int wt[], int val[], int n) 
 { 
-    int dp[n+1][W+1];
-    
-    for(int i = 0 ; i <= n; i++)
-    {
-      for(int j = 0; j <= W; j++)
-      {
-	    	if(i == 0 || j == 0)
-	    	{
-	      		dp[i][j] = 0;
+    	int dp[n+1][W+1];
+	for(int i = 0 ; i <= n; i++)
+    	{
+		for(int j = 0; j <= W; j++)
+      		{
+			if(i == 0 || j == 0)
+			{
+				dp[i][j] = 0;
 			}
 			else if(wt[i-1] <= j)
 			{
@@ -25,3 +24,34 @@ int knapsack(int W, int wt[], int val[], int n)
 	}
 	return dp[n][W];	     
 } 
+-------------------------------------------------------------------------------
+// Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n. Determine the maximum value obtainable by cutting up the rod and selling the pieces. 
+// n - length of rod!..
+// Length array is basically 1,2,3,4,5.. upto n ( NOT ALWAYS NECESSARY! )
+// Prices array basically gives the price of each piece/length of rod.
+// size of length array/price array..
+// Mostly n = size of array or n = size..
+int rod_cut(int n,int prices[],int length[], int size)
+{
+	int dp[size+1][n+1]; // n is length of rod.. size of length of array..
+	for(int i = 0 ; i <= size; i++)
+    	{
+		for(int j = 0; j <= n; j++)
+      		{
+			if(i == 0 || j == 0)
+			{
+				dp[i][j] = 0;
+			}
+			else if(length[i-1] <= j)
+			{
+				dp[i][j] = max(prices[i-1] + dp[i][j-length[i-1]], dp[i-1][j])
+			}
+			else
+			{
+				dp[i][j] = dp[i-1][j];
+			} 
+		}
+	}
+	return dp[size][n];
+}
+	
