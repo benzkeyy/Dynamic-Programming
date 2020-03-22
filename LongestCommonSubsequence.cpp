1910859454b1397b29f50dcd6fb5bb72be0d4423
Harshit <54702967+benzkeyy@users.noreply.github.com>
2020-03-22 21:29:09 +0530

    Added substring and printing LCS concept

--- LongestCommonSubsequence.cpp
+++ LongestCommonSubsequence.cpp
@@ -25,6 +25,51 @@
     }
 
 -----------------------------------------------------------
+ // Printing LCS!
+ -- Write LCS Code..
+ void PrintlongestCommonSubsequence(string text1, string text2) 
+ {
+        int n1 = text1.length(),n2=text2.length();
+        if(n1 == 0 || n2 == 0) return 0;
+        int dp[n1+1][n2+1];
+        for(int i = 0; i <= n1; i++)
+        {
+            for(int j = 0; j <= n2; j++)
+            {
+                if(i == 0 || j == 0) dp[i][j] = 0;
+                
+                else if(text1[i-1] == text2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                
+                else
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        int i = m; int j = n; string s;
+        while(i > 0 && j > 0)
+        {
+           if(text1[i-1] == text2[j-1])
+           {
+              s.push_back(text1[i-1]);
+              i--; j--;
+           }
+           else
+           {
+              if(t[i][j-1] > t[i-1][j])
+              {
+                j--;
+              }
+              else
+              {
+                i--;
+              }
+           }
+        }
+        reverse(s.begin(),s.end());
+       cout << s; // Answer!
+}
+ 
+ -----------------------------------------------------------
  // For substring, we do little modification!
   int longestCommonSubsequence(string text1, string text2) 
     {
